895f4529737a52b10a7d4849c47129a7bc63fe9f
Evan Perry <eperry2688@gmail.com>
2026-01-23 16:13:45 -0700

    30 seconds TTL, remove 30 min

--- frontend/src/features/chat/useChatTtlPickerState.ts
+++ frontend/src/features/chat/useChatTtlPickerState.ts
@@ -4,8 +4,8 @@ export function useChatTtlPickerState() {
   const TTL_OPTIONS = React.useMemo(
     () => [
       { label: 'Off', seconds: 0 },
+      { label: '15 seconds', seconds: 15 },
       { label: '5 min', seconds: 5 * 60 },
-      { label: '30 min', seconds: 30 * 60 },
       { label: '1 hour', seconds: 60 * 60 },
       { label: '6 hours', seconds: 6 * 60 * 60 },
       { label: '1 day', seconds: 24 * 60 * 60 },
